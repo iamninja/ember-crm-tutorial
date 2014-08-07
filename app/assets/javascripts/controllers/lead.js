3f953c272b2085c4f8202fc76d0f061d1b74736c
Vagios Vlachos <bagios234@gmail.com>
2014-08-07 15:14:32 +0300

    Delete leads

--- app/assets/javascripts/controllers/lead.js
+++ app/assets/javascripts/controllers/lead.js
@@ -9,6 +9,14 @@ App.LeadController = Ember.ObjectController.extend({
   actions: {
     saveChanges: function() {
       if (this.get('model.isDirty')) this.get('model').save();
+    },
+
+    delete: function() {
+      var self = this;
+      this.get('model').destroyRecord().then(function() {
+        self.transitionToRoute('leads');
+      });
     }
+
   }
 });
